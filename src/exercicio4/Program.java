a614e95bf3f37cd6b4f6eceef8492678bff398c1
Arthur O. Reis <aor@cesar.school>
2019-03-26 22:00:46 -0300

    commit

--- src/exercicio4/Program.java
+++ src/exercicio4/Program.java
@@ -11,25 +11,10 @@ public class Program {
 		System.out.print("digite numero: ");
 		Estatistica estatistica = new Estatistica(input.nextInt());
 		
-		System.out.println("somatorio[s] media[m] imprimir[i]");
-		System.out.print("escolha opcao: ");
-		char escolha = input.next().charAt(0);
-		
-		if (escolha == 's') {
-			System.out.println("somatorio: " + estatistica.somatorio());
-		}
-		else if (escolha == 'm') {
-			System.out.println("media: " + estatistica.media());
-		}
-		else if (escolha == 'v') {
-			System.out.println("variancia: " + estatistica.variancia());
-		}
-		else if (escolha == 'i') {
-			estatistica.imprimir();
-		}
-		else {
-			System.out.println("escolha invalida");
-		}
+		System.out.println("somatorio: " + estatistica.somatorio());
+		System.out.println("media: " + estatistica.media());
+		System.out.println("variancia: " + estatistica.variancia());
+		estatistica.imprimir();
 		
 		input.close();
 
